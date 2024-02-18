c1dfcf2db3aef90d17ccafa49806b4724ed30d65
Memory-Wabwile <memorywabwile@gmail.com>
2024-02-18 22:14:04 +0300

    add logout functionality

--- app/src/main/java/com/example/chatapplication/MainActivity.kt
+++ app/src/main/java/com/example/chatapplication/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.example.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,13 +20,47 @@ class MainActivity : AppCompatActivity() {
     private lateinit var userList:ArrayList<User>
     private lateinit var adapter: UserAdapter
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().getReference()
 
         userList = ArrayList()
         adapter = UserAdapter(this,userList)
+        // initialize the recycler view
+        userRecyclerView = findViewById(R.id.userRecyclerView)
+
+// set out the layout manager since we are using recycler view
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+
+// set up the adapter
+        userRecyclerView.adapter = adapter
+
+
+
+
     }
+    override fun onCreateOptionsMenu(menu: Menu?):Boolean{
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.logout){
+            // write the logic for logout
+            mAuth.signOut()
+            val  intent = Intent(this@MainActivity, Login::class.java)
+            finish()
+            startActivity(intent)
+            return true
+        }
+        return true
+    }
+
+
+
 }
