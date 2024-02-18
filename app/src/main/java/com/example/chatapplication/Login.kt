3dbd907759c71b44deda5d762ec1caf797dc8e04
Memory-Wabwile <memorywabwile@gmail.com>
2024-02-18 10:48:07 +0300

    debug to ensure firebase auth works

--- app/src/main/java/com/example/chatapplication/Login.kt
+++ app/src/main/java/com/example/chatapplication/Login.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
@@ -41,6 +42,23 @@ class Login : AppCompatActivity() {
 
     }
     private fun login(email:String, password: String){
+        //  logic for logging in user
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // code for logging in user
+                        val intent = Intent(this@Login , MainActivity::class.java)
+                        finish()
+                        startActivity(intent)
+
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this@Login, "User does not exist" , Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
 
     }
 }
--- app/src/main/java/com/example/chatapplication/SignUp.kt
+++ app/src/main/java/com/example/chatapplication/SignUp.kt
@@ -1,10 +1,13 @@
 package com.example.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+
 
 class SignUp : AppCompatActivity() {
     private lateinit var edtName : EditText
@@ -16,15 +19,41 @@ class SignUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        mAuth = FirebaseAuth.getInstance()
+
         //  initializing the views
         edtName = findViewById(R.id.edt_name)
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
         btnSignUp = findViewById(R.id.btnSignUp)
 
-        //sign up user when button is clicked
+        // when button is clicked getting the email and password
         btnSignUp.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.toString()
 
+            // creating/signing up the new user with email and password
+            signUp(email,password)
         }
+    }
+    private fun signUp(email:String, password:String){
+        //logic of creating user
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // code for redirecting to home page
+                    val intent = Intent(this@SignUp , MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+
+
+                } else {
+                    // if signup is unsuccessfull
+                    Toast.makeText(this@SignUp, "Some error occured", Toast.LENGTH_SHORT).show()
+
+
+                }
+            }
+
     }
 }
